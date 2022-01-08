319dc32cc5b3cf7047f272c307b9cceccc7807db
shikomatlala <52504166+shikomatlala@users.noreply.github.com>
2022-01-08 03:34:52 +0200

    Student view added

---
+++ user/student/view/index.php
@@ -0,0 +1,13 @@
+<?php
+    include_once "../../../config/connect.php";
+?>
+<!-- <button onclick="window.open(window.print(document.querySelector('page').outerHTML))">Print</button> -->
+<!-- <button onclick="window.open(window.print(document.querySelector('page').outerHTML))">Print</button> -->
+<button onclick="window.open('letter.php')">Download</button>
+
+<!-- <page size="A4"></page>
+<page size="A4" layout="landscape"></page>
+<page size="A5"></page>
+<page size="A5" layout="landscape"></page>
+<page size="A3"></page>
+<page size="A3" layout="landscape"></page> -->
---
+++ user/student/view/letter.php
@@ -0,0 +1,85 @@
+<?php
+    include_once "../../../config/connect.php";
+?>
+<page size="A4" >
+    <div id="letterLogo" class="letterLogo">
+        <img height="auto" width="250" src="../../../media/WitLogo.svg"/>
+    </div>
+    <div id="facultyInformation" class="facultyInformation">
+        <p>Facutly Information and Communication Technology<br>
+        (SOSANGUVE CAMPUS) - Computer Science Department</p>
+    </div>
+    <div id="enquiryDiv" class="enquiryDiv">
+        <div id="enqName" class="enqDiv">
+            <br>
+            <p>Enquiries: Mr. VN Ranko &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                E-mail: <a href="#">rankovn@tut.ac.za</a><br>
+                Tel: 012-382 9112/9938 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Private Bag X07, Pretoria North, 0116 </p>
+        </div>
+    </div>
+    <div id="hrDiv" class="hrDiv">
+        <hr>
+    </div>
+    <div id="letterContent" class="letterContent">
+        <div id="recipientTitle" class="recepientTitle">
+            <h1>TO WHOM IT MAY CONCERN</h1>
+        </div>
+        <div id="letterBody" class="letterBody">
+            <p>
+                The letter certify that [<b>Mr. Matlala S.B</b> (Student number <b>216955960</b>)] is a potential graduate for [National Diploma in IT: Software Development] from the Tshwane University of Technology.
+                <br><br>
+                It is my pleasure to recommend [Mr. Matlala S.B] for a work integrated learning exposure as it is a requirement for the student to complete the diploma
+                <br><br>
+                The major fields of study in this diploma are:
+                <ul>
+                    <li>   Software Development</li>
+                    <li>   Mobile Development</li>
+                    <li>   Business Analysis and Modelling</li>
+                    <li>   Software Testing</li>
+                    <li>    Database Development and Administration</li>
+                </ul>
+
+                For the experiential training and add value to the student, we suggest that the student be assigned tasks and responsibilities that are aligned to [his/her] field of study. It is to be noted that the above mentioned fields of study are only a guideline in this regard, any other Information Technology(IT) oriented projects and tasks may also be accepted. The duration of the industry exposure should be a minimum of <b>6 months</b>.
+                <br><br>
+                The student has consistently demonstrated an ability to rise to any challenges that he may face in [his/her] academic students or any other endeavour.
+                <br><br>
+                In conclusion, I would like to sate my strong recommendation for <b>[Mr. Matlala S.B]</b> if you have any further question regarding [his/her] ability or this recommendation, please do not hesitate to contact me or my office using the information on this letter head
+            </p>
+        </div>
+    </div>
+    <div id="signatureDiv" class="signature">
+            <img height="auto" width="120px" src="../../../media/signature/signature1.png">
+    </div>
+    <div id="footerDetails" class="footerDiv">
+        <p>..................................<br>
+            Kind Regards,<br>
+            Victor Nkhumise Ranko<br>
+            Lecturer and WIL Coordinator<br>
+            Department: Computer Science<br>
+            Faculty: ICT<br>
+            Building 20-115<br>
+            Soshanguve South Campus<br>
+            Tel: 012 382 9112
+        </p>
+    </div>
+    <div id="stamp" class="stamp">
+        <div id="stamp-border" class="stamp-border">
+            <img height="auto" width="auto" src="../../../media/stamp/border.svg">
+            <div id="stamp-logo" class="stamp-logo">
+                <img height="auto" width="auto" src="../../../media/stamp/stampLogo.svg">
+            </div>
+            <div id="stamp-dept-title" class="stamp-dept-title">
+                <img height="auto" width="auto" src="../../../media/stamp/departmentTitle.svg">
+            </div>
+            <div id="stamp-dept-name" class="stamp-dept-name">
+                <img height="auto" width="auto" src="../../../media/stamp/departments/computer-science.svg">
+            </div>
+            <div id="stamp-fac-title" class="stamp-fac-title">
+                <img height="auto" width="auto" src="../../../media/stamp/facultyTitle.svg">
+            </div>
+            <div id="stamp-fac-name" class="stamp-fac-name">
+                <img height="auto" width="auto" src="../../../media/stamp/faculties/ict.svg">
+            </div>
+        </div>
+    </div>
+</page>
