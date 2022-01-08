e1e7db5963311a02e17a6e86901213fff73163a7
shikomatlala <52504166+shikomatlala@users.noreply.github.com>
2022-01-08 03:36:40 +0200

    Index File added

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "config/connect.php";
+?>
+
+<page size="A4"></page>
+<!-- <page size="A4"></page>
+<page size="A4" layout="landscape"></page>
+<page size="A5"></page>
+<page size="A5" layout="landscape"></page>
+<page size="A3"></page>
+<page size="A3" layout="landscape"></page> -->
